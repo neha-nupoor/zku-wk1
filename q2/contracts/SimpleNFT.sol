c7c07633a7209fa604529c2b513b69dee6da8667
Neha Nupoor <88262104+neha-nupoor@users.noreply.github.com>
2022-03-06 20:20:15 +0530

    Update SimpleNFT.sol

--- q2/contracts/SimpleNFT.sol
+++ q2/contracts/SimpleNFT.sol
@@ -2,15 +2,13 @@
 // contracts/SimpleNFT.sol
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
-import "../contracts/MerkleTree.sol";
 
-contract SimpleNFT is ERC721Full {
+contract SimpleNFT is ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter; // ??
     Counters.Counter private _tokenIds; // setting up _tokenIds which is Counters.Counter type.
